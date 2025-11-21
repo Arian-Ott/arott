1842c0d28bf3c10b8916e97a3122a119e44ec414
Arian-Ott <arian.ott@ieee.org>
2025-11-21 08:41:29 +0000

    Added types.h

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,6 @@
+#include "types.h"
+
+
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
@@ -13,11 +16,11 @@ extern "C" void callConstructors()
 
 void printf(char* str)
 {
-    unsigned short*VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t*VideoMemory = (uint16_t*)0xb8000;
     for(int i = 0; str[i] != '\0'; i++)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World! - By Arian Ott 2");
     while(1);
